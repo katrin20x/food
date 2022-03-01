aa5bd282e30f8961600b9e7e52ea9248e4dc1b79
Ekaterina <katrin_work@mail.ru>
2022-03-01 20:47:13 +0300

    made Modal general part

--- js/script.js
+++ js/script.js
@@ -103,4 +103,46 @@ function setClock(selector, endtime) {
 
 setClock('.timer', deadline);
 
+
+//Modal
+
+const modalTrigger = document.querySelectorAll('[data-modal]'),
+      modalCloseBtn = document.querySelector('[data-close]'),
+      modal = document.querySelector('.modal');
+
+
+      function openModal() {
+        
+           modal.classList.add('show');
+           modal.classList.remove('hide');
+           document.body.style.overflow = 'hidden';
+          }
+
+      modalTrigger.forEach(btn => {
+
+        btn.addEventListener('click', openModal);
+
+      });
+
+      function closeModal () {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+
+      }
+      
+
+      modalCloseBtn.addEventListener('click', closeModal);
+
+      modal.addEventListener('click', (e) => {
+          if (e.target === modal){
+            closeModal();
+          }
+      });
+
+      document.addEventListener('keydown', (e) => {
+          if (e.code === 'Escape' && modal.classList.contains('show')) {
+              closeModal();
+          }
+      });
 });
